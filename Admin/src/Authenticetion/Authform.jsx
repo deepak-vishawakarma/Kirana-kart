07c0b67e7e30b2a4d416c5001a7ef87c48016e19
Deepak Vishwakarma <vishwakarmad2337@gmail.com>
2025-07-27 20:12:33 +0530

    Update Authform.jsx

--- Admin/src/Authenticetion/Authform.jsx
+++ Admin/src/Authenticetion/Authform.jsx
@@ -77,6 +77,7 @@ export default function Authform() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter password"
           />
+           <p className='w-full px-3 py-2'>Maximum 8 Characters</p>
         </div>
 
         <button
@@ -149,6 +150,7 @@ export default function Authform() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter password"
           />
+           <p className='w-full px-3 py-2'>Maximum 8 Characters</p>
         </div>
 
         <button
